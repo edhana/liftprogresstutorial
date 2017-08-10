4d0cbda614418c012ff55e52e798b891406fb4f5
Eduardo Marques <edhana@gmail.com>
2017-08-10 19:11:18 -0300

    Fixed the javascript configuration adding jquery and tether scripts

--- Gemfile
+++ Gemfile
@@ -51,5 +51,10 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 # Added Gems
+gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
 gem 'bootstrap', '~> 4.0.0.alpha6'
 gem 'react-rails'
+
+source 'https://rails-assets.org' do
+  gem 'rails-assets-tether', '>= 1.3.3'
+end
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require rails-ujs
 //= require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require tether
 //= require bootstrap-sprockets
 //= require react
 //= require react_ujs
